831ab66b5d87fecdfbadd0e5b87ba3e46ab86a25
hoangNH <hoangcdnbk@gmail.com>
2025-03-31 20:36:36 +0700

    create project

---
+++ src/main/java/com/codegym/airline_tickets/AirlineTicketsApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.airline_tickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineTicketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineTicketsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codegym/airline_tickets/AirlineTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.airline_tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineTicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
