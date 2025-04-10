7b33beb26c1106c78f33185d2bf161816e757bee
NgocLe999 <108927715+NgocLe999@users.noreply.github.com>
2025-04-10 21:25:24 +0700

    9:25PM - 10/04/2025

--- src/main/resources/static/user/js/homepage.js
+++ src/main/resources/static/user/js/homepage.js
@@ -41,7 +41,42 @@
     if (counts.child > 0) passengerText.push(`${counts.child} Trẻ em`);
     if (counts.infant > 0) passengerText.push(`${counts.infant} Em bé`);
 
-    document.getElementById('passengerInput').value = passengerText.join(", ") || "Chọn số hành khách";
-    let dropdown = bootstrap.Dropdown.getInstance(document.getElementById('passengerInput'));
+    document.getElementById('passengers').value = passengerText.join(", ") || "Chọn số hành khách";
+    let dropdown = bootstrap.Dropdown.getInstance(document.getElementById('passengers'));
     if (dropdown) dropdown.hide();
 }
+
+    function submitFormSearch(e){
+        event.preventDefault();
+
+        sessionStorage.setItem("data","data");
+
+            let searchFormData = {
+                departureAirport: null,
+                arrivalAirport: null,
+                departureTime: null,
+                arrivalTime: null,
+                departureOneWay: null,
+            };
+
+                const inputDeparture = document.getElementById("departure")
+                searchFormData.departureAirport = inputDeparture.value
+
+                const inputArrival = document.getElementById("arrival")
+                searchFormData.arrivalAirport = inputArrival.value
+
+                const inputDepartureDate = document.getElementById("departure-date")
+                searchFormData.departureTime = inputDepartureDate.value
+
+                const inputArrivalDate = document.getElementById("arrival-date")
+                searchFormData.arrivalTime = inputArrivalDate.value
+
+                const inputDepartureOneWay = document.getElementById("destination-one-way")
+                searchFormData.departureOneWay = inputDepartureOneWay.value
+
+                sessionStorage.setItem("data",JSON.stringify(searchFormData));
+
+                document.getElementById("searchForm").submit();
+
+    }
+
