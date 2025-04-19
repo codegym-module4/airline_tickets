9e5460946c8d5b6ee1eaf62fa3c13335a681646b
LinhChi2511 <chidao2511@gmail.com>
2025-04-19 21:05:44 +0700

    loi

--- src/main/resources/static/user/js/homepage.js
+++ src/main/resources/static/user/js/homepage.js
@@ -37,7 +37,7 @@ document.addEventListener("DOMContentLoaded", function () {
         arrivalDateOption.classList.add("d-block")
 
 
-});
+    });
 
     oneWayRadio.addEventListener("click", function () {
         const inputRoundTrip = document.getElementById("arrival")
@@ -50,24 +50,24 @@ document.addEventListener("DOMContentLoaded", function () {
         roundTripSection.classList.add("d-none");
         arrivalDateOption.classList.add("d-none")
 
-});
-
-});
-
-
-    window.addEventListener("load", (event) => {
-        // document.getElementById('passengers').value = "1 Người lớn"
-
-        const storageDefault = JSON.parse(window.sessionStorage.getItem("data"))
-
-        const inputOw = document.querySelector(".des-one-way")
-        inputOw.value = storageDefault.arrivalOneWay
-
-        // sessionStorage.clear()
-
     });
 
-    const changeCount = (event, type, value) => {
+});
+
+
+window.addEventListener("load", (event) => {
+    // document.getElementById('passengers').value = "1 Người lớn"
+
+    const storageDefault = JSON.parse(window.sessionStorage.getItem("data"))
+
+    const inputOw = document.querySelector(".des-one-way")
+    inputOw.value = storageDefault.arrivalOneWay
+
+    // sessionStorage.clear()
+
+});
+
+const changeCount = (event, type, value) => {
     event.stopPropagation();
     event.preventDefault();
     let element = document.getElementById(type);
@@ -86,16 +86,16 @@ document.addEventListener("DOMContentLoaded", function () {
     if (count < 0) count = 0;
     element.textContent = count;
 
-        }
+}
 
 
 
-    const submitForm = (e) => {
+const submitForm = (e) => {
     e.preventDefault();
     let counts = {
-    adult: parseInt(document.getElementById('adults').textContent),
-    child: parseInt(document.getElementById('child').textContent),
-    infant: parseInt(document.getElementById('infant').textContent)
+        adult: parseInt(document.getElementById('adults').textContent),
+        child: parseInt(document.getElementById('child').textContent),
+        infant: parseInt(document.getElementById('infant').textContent)
     };
 
     let passengerText = [];
@@ -106,54 +106,54 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById('passengers').value = passengerText.join(", ") || "Chọn số hành khách";
     let dropdown = bootstrap.Dropdown.getInstance(document.getElementById('passengers'));
     if (dropdown) dropdown.hide();
+}
+
+const submitFormSearch = (e) =>{
+    event.preventDefault();
+
+    const checkedRoundTrip = document.getElementById("round-trip")
+    searchFormData.isRoundTrip= checkedRoundTrip.checked
+
+    const checkedOneWay = document.getElementById("one-way")
+    searchFormData.isOneWay= checkedOneWay.checked
+
+    const inputDeparture = document.getElementById("departure")
+    searchFormData.departureAirport = inputDeparture.value
+
+    if(searchFormData.isRoundTrip === true){
+        const inputArrival = document.getElementById("arrival")
+        searchFormData.arrivalAirport = inputArrival.value
     }
 
-    const submitFormSearch = (e) =>{
-        event.preventDefault();
-
-                const checkedRoundTrip = document.getElementById("round-trip")
-                searchFormData.isRoundTrip= checkedRoundTrip.checked
-
-                const checkedOneWay = document.getElementById("one-way")
-                searchFormData.isOneWay= checkedOneWay.checked
-
-                const inputDeparture = document.getElementById("departure")
-                searchFormData.departureAirport = inputDeparture.value
-
-                if(searchFormData.isRoundTrip === true){
-                        const inputArrival = document.getElementById("arrival")
-                        searchFormData.arrivalAirport = inputArrival.value
-                }
-
-                if(  searchFormData.isOneWay === true){
-                        const inputArrivalOneWay = document.querySelector(".des-one-way")
-                        searchFormData.arrivalOneWay = inputArrivalOneWay.value
-                    }
-
-                const inputDepartureDate = document.getElementById("departure-date")
-                searchFormData.departureTime = inputDepartureDate.value
-
-                const inputArrivalDate = document.getElementById("arrival-date")
-                searchFormData.arrivalTime = inputArrivalDate.value
-
-                const inputAdults = document.getElementById("adults");
-                searchFormData.quantity.adult = parseInt(inputAdults.textContent)
-
-                const inputChild = document.getElementById("child");
-                searchFormData.quantity.child = parseInt(inputChild.textContent)
-
-                const inputInfant = document.getElementById("infant");
-                searchFormData.quantity.infant = parseInt(inputInfant.textContent)
-
-
-                sessionStorage.setItem("data",JSON.stringify(searchFormData));
-
-                document.getElementById("searchForm").submit();
-
+    if(  searchFormData.isOneWay === true){
+        const inputArrivalOneWay = document.querySelector(".des-one-way")
+        searchFormData.arrivalOneWay = inputArrivalOneWay.value
     }
 
+    const inputDepartureDate = document.getElementById("departure-date")
+    searchFormData.departureTime = inputDepartureDate.value
 
-    $(document).ready( () => {
+    const inputArrivalDate = document.getElementById("arrival-date")
+    searchFormData.arrivalTime = inputArrivalDate.value
+
+    const inputAdults = document.getElementById("adults");
+    searchFormData.quantity.adult = parseInt(inputAdults.textContent)
+
+    const inputChild = document.getElementById("child");
+    searchFormData.quantity.child = parseInt(inputChild.textContent)
+
+    const inputInfant = document.getElementById("infant");
+    searchFormData.quantity.infant = parseInt(inputInfant.textContent)
+
+
+    sessionStorage.setItem("data",JSON.stringify(searchFormData));
+
+    document.getElementById("searchForm").submit();
+
+}
+
+
+$(document).ready( () => {
     $('.hot-deal-link').on('click', function (e) {
         e.preventDefault();
 
@@ -179,8 +179,3 @@ document.addEventListener("DOMContentLoaded", function () {
 
     });
 });
-
-
-
-
-
